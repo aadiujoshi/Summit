de3a1cd9acb42379c01574c9aaf04d882d7ab93f
Adi B <adib09132005@gmail.com>
2022-11-13 15:45:36 -0600

    fix path

--- Summit/src/summit/Main.java
+++ Summit/src/summit/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Thread main = new Thread(new Runnable() {
             @Override
             public void run() {
-                BufferedSprites.loadSprites("/Users/adi/Documents/GitHub/Summit/Summit/src/summit/resources");
+                BufferedSprites.loadSprites("Summit/src/summit/resources");
 
                 java.awt.Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 
