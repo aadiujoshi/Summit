a93e92ff5773d5c84efe25fa1c47aa62628757ca
Adi B <adib09132005@gmail.com>
2022-11-14 12:10:18 -0600

    added new entity classes

---
+++ Summit/src/summit/game/entity/Arrow.java
@@ -0,0 +1,47 @@
+package summit.game.entity;
+
+import java.awt.event.MouseEvent;
+
+import summit.game.GameMap;
+import summit.game.GameUpdateEvent;
+import summit.gfx.PaintEvent;
+
+public class Arrow extends Projectile {
+
+    public Arrow(float x, float y, float width, float height) {
+        super(x, y, width, height);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void paint(PaintEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void gameClick(GameMap map, MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void damage(GameUpdateEvent ge, Entity e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void destroy(GameUpdateEvent ge) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void collide(Entity e) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+
+}
---
+++ Summit/src/summit/game/entity/Projectile.java
@@ -0,0 +1,8 @@
+package summit.game.entity;
+
+public abstract class Projectile extends Entity {
+
+    public Projectile(float x, float y, float width, float height) {
+        super(x, y, width, height);
+    }
+}
