23983437d841c4c8d668e96d5694b3c247eeb05a
Adi <adib09132005@gmail.com>
2023-01-01 13:52:47 -0600

    start documenting gui

--- Summit/src/summit/gui/TextContainer.java
+++ Summit/src/summit/gui/TextContainer.java
@@ -1,7 +1,4 @@
-/*
-* BPA project by Aadi Joshi, Aditya Bhattacharya, Sanjay Raghav, Aadithya Ramakrishnan Sriram 
-* 2022
-*/
+
 package summit.gui;
 
 import summit.gfx.Renderer;
@@ -12,57 +9,94 @@ import java.util.List;
 import summit.gfx.ColorFilter;
 import summit.gfx.PaintEvent;
 
-public class TextContainer extends Container{
+/**
+ * This class represents a container for displaying text. 
+ * 
+ * @author Aadi J, Aditya B, Sanjay R, Aadithya R. S.
+ */
+public class TextContainer extends Container {
 
     private String text;
     private ColorFilter textFilter;
 
-    public TextContainer(String text, Container parent, 
-                        Window window, 
-                        float relX, float relY, 
-                        String sprite) {
+    /**
+     * Constructs a TextContainer with the given text, parent container, window,
+     * relative x and y position, and sprite.
+     * 
+     * @param text   the text to be displayed
+     * @param parent the parent container of this container
+     * @param window the window that this container belongs to
+     * @param relX   the relative x position of this container
+     * @param relY   the relative y position of this container
+     * @param sprite the sprite to be used for this container
+     */
+    public TextContainer(String text, Container parent,
+            Window window,
+            float relX, float relY,
+            String sprite) {
         super(parent, window, relX, relY, sprite);
         this.text = text;
         textFilter = new ColorFilter(0xffffff);
     }
 
     @Override
-    public void paint(PaintEvent e){
+    public void paint(PaintEvent e) {
         super.paint(e);
 
         List<String> lines = new ArrayList<>();
-        
+
         int k = 0;
 
         lines.add("");
 
-        for(int i = 0; i < text.length(); i++){
-            if(text.charAt(i) == ' '){
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == ' ') {
                 k++;
                 lines.add("");
                 continue;
             }
 
-            lines.set(k, lines.get(k)+text.charAt(i));
+            lines.set(k, lines.get(k) + text.charAt(i));
         }
 
         for (int i = 0; i < lines.size(); i++) {
-            e.getRenderer().renderText(lines.get(i), (int)getX(), (int)getY()+(i*9)-(lines.size()/2*4), Renderer.NO_OP, textFilter);
+            e.getRenderer().renderText(lines.get(i), (int) getX(), (int) getY() + (i * 9) - (lines.size() / 2 * 4),
+                    Renderer.NO_OP, textFilter);
         }
     }
-    
+
+    /**
+     * Returns the text displayed by this container.
+     * 
+     * @return the text displayed by this container
+     */
     public String getText() {
         return this.text;
     }
 
+    /**
+     * Sets the text displayed by this container.
+     * 
+     * @param text the text to be displayed by this container
+     */
     public void setText(String text) {
         this.text = text;
     }
-    
+
+    /**
+     * Returns the color filter applied to the text.
+     * 
+     * @return the color filter applied to the text
+     */
     public ColorFilter getTextFilter() {
         return this.textFilter;
     }
 
+    /**
+     * Sets the color filter applied to the text.
+     * 
+     * @param textFilter the color filter to be applied to the text
+     */
     public void setTextFilter(ColorFilter textFilter) {
         this.textFilter = textFilter;
     }
--- Summit/src/summit/gui/TransitionScreen.java
+++ Summit/src/summit/gui/TransitionScreen.java
@@ -4,15 +4,32 @@ import summit.gfx.PaintEvent;
 import summit.gfx.Renderer;
 import summit.gfx.Sprite;
 
-public class TransitionScreen extends TextContainer{
+/**
+ * A TransitionScreen is a non-navigable container that displays a message over
+ * a solid colored background. The TransitionScreen is typically used to convey
+ * a change in the application state, such as loading or transitioning between
+ * menus.
+ * 
+ * @author Aadi J, Aditya B, Sanjay R, Aadithya R. S.
+ *
+ */
+public class TransitionScreen extends TextContainer {
 
+    /**
+     * Constructs a new TransitionScreen with the specified text, window, and
+     * background color.
+     *
+     * @param window the parent window of this TransitionScreen
+     * @param text   the text to be displayed on the TransitionScreen
+     */
     public TransitionScreen(Window window, String text) {
         super(text, null, window, 0.5f, 0.5f, Sprite.FILL_SCREEN);
         super.setNavContainer(false);
     }
 
+
     @Override
-    public void paint(PaintEvent e){
+    public void paint(PaintEvent e) {
         e.getRenderer().fillRect(0, 0, Renderer.WIDTH, Renderer.HEIGHT, 0x254aff);
         super.paint(e);
     }
