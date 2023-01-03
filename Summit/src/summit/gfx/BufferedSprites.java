3018258523b72dddfbaef232f73843c05dbf6855
Adi <adib09132005@gmail.com>
2023-01-02 23:06:25 -0600

    unsaved files now saved

--- Summit/src/summit/gfx/BufferedSprites.java
+++ Summit/src/summit/gfx/BufferedSprites.java
@@ -1,7 +1,4 @@
-/*
-* BPA project by Aadi Joshi, Aditya Bhattacharya, Sanjay Raghav, Aadithya Ramakrishnan Sriram 
-* 2022
-*/
+
 package summit.gfx;
 
 import java.awt.image.BufferedImage;
@@ -13,53 +10,73 @@ import javax.imageio.ImageIO;
 
 import summit.Main;
 
-public class BufferedSprites{
+/**
+ * BufferedSprites is a utility class that loads and stores image sprites in a
+ * HashMap
+ * 
+ * @author Aadi J, Aditya B, Sanjay R, Aadithya R. S.
+ */
+public class BufferedSprites {
 
+    /**
+     * A HashMap that stores the sprites, with the keys being the names of the
+     * sprites and the values being the
+     * pixel data for the sprites.
+     */
     private static HashMap<String, int[][]> sprites = new HashMap<>();
 
-    public final static char[] charRefs = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                                                '!', '/', '<', '>', ':', '+', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '(', ')', '.', '?'};
+    /**
+     * An array of characters that correspond to the characters in the text sheet.
+     */
+    public final static char[] charRefs = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            '!', '/', '<', '>', ':', '+', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '(', ')', '.',
+            '?' };
 
-    public static void loadSprites(){
-        
+    /**
+     * Loads all the sprites in the resources/sprites directory and stores them in
+     * the sprites HashMap.
+     */
+    public static void loadSprites() {
+
         File[] dirFiles = new File(Main.path + "resources/sprites").listFiles();
 
-        for(int i = 0; i < dirFiles.length; i++){
+        for (int i = 0; i < dirFiles.length; i++) {
             String absPath = dirFiles[i].getAbsolutePath();
-            
-            if(absPath.contains(".png") || absPath.contains(".jpg")){
-                try{
+
+            if (absPath.contains(".png") || absPath.contains(".jpg")) {
+                try {
                     String ref = dirFiles[i].getName().substring(0, dirFiles[i].getName().lastIndexOf("."));
 
                     BufferedImage sprite = ImageIO.read(dirFiles[i]);
 
                     int[][] dataBuffer = new int[sprite.getHeight()][sprite.getWidth()];
 
-                    if(ref.equals("textsheet")){
+                    if (ref.equals("textsheet")) {
                         int ind = 0;
-                        for(int r = 0; r < dataBuffer.length; r+=8){
-                            for(int c = 0; c < dataBuffer[0].length; c+=8){
-                                
+                        for (int r = 0; r < dataBuffer.length; r += 8) {
+                            for (int c = 0; c < dataBuffer[0].length; c += 8) {
+
                                 int[][] charSprite = new int[8][8];
 
-                                for(int r1 = r; r1 < r+8; r1++){
-                                    for(int c1 = c; c1 < c+8; c1++){
+                                for (int r1 = r; r1 < r + 8; r1++) {
+                                    for (int c1 = c; c1 < c + 8; c1++) {
                                         int argb = sprite.getRGB(c1, r1);
-                                
-                                        if((argb >> 24) != 0){
-                                            //make text black
+
+                                        if ((argb >> 24) != 0) {
+                                            // make text black
                                             argb &= 0b01010101000000000000000000000000;
                                         }
-                                        if((argb >> 24) == 0){
+                                        if ((argb >> 24) == 0) {
                                             argb = -1;
                                         }
 
-                                        charSprite[r1-r][c1-c] = argb;
-                                    }   
+                                        charSprite[r1 - r][c1 - c] = argb;
+                                    }
                                 }
 
-                                sprites.put( ( charRefs[ind] + Sprite.TEXT_APPEND_KEY ), charSprite);
-                                
+                                sprites.put((charRefs[ind] + Sprite.TEXT_APPEND_KEY), charSprite);
+
                                 ind++;
 
                             }
@@ -68,32 +85,42 @@ public class BufferedSprites{
                         for (int r = 0; r < dataBuffer.length; r++) {
                             for (int c = 0; c < dataBuffer[0].length; c++) {
                                 int argb = sprite.getRGB(c, r);
-                                
-                                if((argb >> 24) != 0){
+
+                                if ((argb >> 24) != 0) {
                                     argb &= 0b01010101111111111111111111111111;
                                 }
-                                if((argb >> 24) == 0){
+                                if ((argb >> 24) == 0) {
                                     argb = -1;
                                 }
-                                
+
                                 dataBuffer[r][c] = argb;
                             }
                         }
 
                         sprites.put(ref, dataBuffer);
-    
+
                         System.out.println(ref);
                     }
-                    
-                } catch(IOException e) {}
+
+                } catch (IOException e) {
+                }
             }
         }
     }
 
-    public static int[][] getSprite(String spriteInd){
-        if(spriteInd == null)
-            return new int[][]{{-1}};
+    /**
+     * Returns the sprite with the specified name. If the sprite does not exist,
+     * returns a sprite with a single
+     * white pixel.
+     * 
+     * @param spriteInd the name of the sprite to retrieve
+     * @return the sprite with the specified name
+     */
 
-        return (int[][])sprites.get(spriteInd);
+    public static int[][] getSprite(String spriteInd) {
+        if (spriteInd == null)
+            return new int[][] { { -1 } };
+
+        return (int[][]) sprites.get(spriteInd);
     }
 }
--- Summit/src/summit/gfx/Camera.java
+++ Summit/src/summit/gfx/Camera.java
@@ -1,45 +1,87 @@
-/*
-* BPA project by Aadi Joshi, Aditya Bhattacharya, Sanjay Raghav, Aadithya Ramakrishnan Sriram 
-* 2022
-*/
 package summit.gfx;
 
 import java.io.Serializable;
 
-public class Camera implements Serializable, Cloneable{
+/**
+ * The Camera class represents the camera in a 2D game, with x and y position
+ * coordinates. It implements the
+ * Serializable and Cloneable interfaces to allow for saving and copying of
+ * camera objects.
+ * 
+ * @author Aadi J, Aditya B, Sanjay R, Aadithya R. S.
+ */
+public class Camera implements Serializable, Cloneable {
 
+    /** The x position coordinate of the camera. */
     private float x;
+
+    /** The y position coordinate of the camera. */
     private float y;
 
-    public Camera(float x, float y){
+    /**
+     * Constructs a Camera object with the specified x and y position coordinates.
+     * 
+     * @param x the x position coordinate of the camera
+     * @param y the y position coordinate of the camera
+     */
+    public Camera(float x, float y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Returns the x position coordinate of the camera.
+     * 
+     * @return the x position coordinate of the camera
+     */
     public float getX() {
         return this.x;
     }
 
+    /**
+     * Sets the x position coordinate of the camera.
+     * 
+     * @param x the new x position coordinate of the camera
+     */
     public void setX(float x) {
         this.x = x;
     }
 
+    /**
+     * Returns the y position coordinate of the camera.
+     * 
+     * @return the y position coordinate of the camera
+     */
     public float getY() {
         return this.y;
     }
 
+    /**
+     * Sets the y position coordinate of the camera.
+     * 
+     * @param y the new y position coordinate of the camera
+     */
     public void setY(float y) {
         this.y = y;
     }
 
-    
+    /**
+     * Returns a string representation of the Camera object, in the format "x y".
+     * 
+     * @return a string representation of the Camera object
+     */
     @Override
-    public String toString(){
+    public String toString() {
         return this.x + "  " + this.y;
     }
 
-    @Override 
-    public Camera clone(){
+    /**
+     * Returns a deep copy of the Camera object.
+     * 
+     * @return a deep copy of the Camera object
+     */
+    @Override
+    public Camera clone() {
         return new Camera(getX(), getY());
     }
 }
