4a7b3c34fb08cb376e43173f6083dd71a35a466b
Adi <adib09132005@gmail.com>
2023-01-05 14:36:47 -0600

    complete AI documentation

--- Summit/src/summit/game/ai/EntityAI.java
+++ Summit/src/summit/game/ai/EntityAI.java
@@ -1,7 +1,3 @@
-/*
-* BPA project by Aadi Joshi, Aditya Bhattacharya, Sanjay Raghav, Aadithya Ramakrishnan Sriram 
-* 2022
-*/
 package summit.game.ai;
 
 import java.io.Serializable;
@@ -13,86 +9,145 @@ import summit.util.ScheduledEvent;
 import summit.util.Time;
 
 /**
- * Controls the movement of the parent Entity
+ * 
+ * Controls the movement of the parent Entity.
+ * 
+ * This class is used to control the movement of an Entity object. It can be
+ * used to make the entity wander around randomly or move towards a specific
+ * destination.
+ * 
+ * @author Aadi J, Aditya B, Sanjay R, Aadithya R.
  */
-public class EntityAI implements Serializable{
 
+public class EntityAI implements Serializable {
+
+    /** The Entity object that this EntityAI controls. */
     protected Entity entity;
-    
+
+    /** The destination x-coordinate for the Entity. */
     private float destX;
+
+    /* The destination y-coordinate for the Entity. */
     private float destY;
 
+    /** Whether the Entity has reached its destination. */
     private boolean reachedDest;
 
+    /** A ScheduledEvent used to make the Entity wander. */
     private ScheduledEvent wandering;
 
-    public EntityAI(Entity e){
+    /**
+     * 
+     * Constructs a new EntityAI for the specified Entity.
+     * 
+     * @param e the Entity object that this EntityAI will control
+     */
+    public EntityAI(Entity e) {
         this.entity = e;
     }
 
-    public void wander(boolean w){
-        if(!w && wandering != null){
-            //remove from scheduler
+    /**
+     * 
+     * Makes the Entity wander around randomly.
+     * 
+     * If the parameter is true, the Entity will start wandering around randomly. If
+     * it is false, the Entity will
+     * 
+     * stop wandering. The Entity will choose a new destination every 5-15 seconds
+     * and will continue to do so until
+     * 
+     * the wander method is called with a false parameter.
+     * 
+     * @param w whether the Entity should start wandering
+     */
+    public void wander(boolean w) {
+        if (!w && wandering != null) {
+            // remove from scheduler
             this.wandering.manualTerminate();
             this.wandering = null;
         }
 
-        //only start once
-        if(w && wandering == null){
-            destX = entity.getX() + (float)(Math.random()*10-5);
-            destY = entity.getY() + (float)(Math.random()*10-5);
+        // only start once
+        if (w && wandering == null) {
+            destX = entity.getX() + (float) (Math.random() * 10 - 5);
+            destY = entity.getY() + (float) (Math.random() * 10 - 5);
 
-            //1 second
+            // 1 second
             final long S = Time.MS_IN_S;
 
-            this.wandering = new ScheduledEvent((long)(Math.random()*(10*S) + (5*S)), ScheduledEvent.FOREVER) {
+            this.wandering = new ScheduledEvent((long) (Math.random() * (10 * S) + (5 * S)), ScheduledEvent.FOREVER) {
                 @Override
                 public void run() {
                     this.manualTerminate();
 
-                    destX = entity.getX() + (float)(Math.random()*10-5);
-                    destY = entity.getY() + (float)(Math.random()*10-5);
+                    destX = entity.getX() + (float) (Math.random() * 10 - 5);
+                    destY = entity.getY() + (float) (Math.random() * 10 - 5);
 
-                    this.setDelayMS((long)(Math.random()*(10*S) + (5*S)));
+                    this.setDelayMS((long) (Math.random() * (10 * S) + (5 * S)));
                 }
             };
 
             GameScheduler.registerEvent(wandering);
         }
     }
-    
-    public void next(GameUpdateEvent e){
-        float delta_x = entity.getDx()/Time.NS_IN_S * e.getDeltaTimeNS();
-        float delta_y = entity.getDy()/Time.NS_IN_S * e.getDeltaTimeNS();
 
-        delta_x *= (destX < entity.getX()) ? -1 : 1; 
-        delta_y *= (destY < entity.getY()) ? -1 : 1; 
+    /**
+     * 
+     * Moves the Entity towards its destination.
+     * This method should be called every game tick to update the Entity's position.
+     * The Entity will move towards
+     * its destination at a speed determined by its dx and dy values. If the Entity
+     * has reached its destination,
+     * the reachedDest field will be set to true.
+     * 
+     * @param e the GameUpdateEvent for the current game tick
+     * @see GameUpdateEvent
+     */
+    public void next(GameUpdateEvent e) {
+        float delta_x = entity.getDx() / Time.NS_IN_S * e.getDeltaTimeNS();
+        float delta_y = entity.getDy() / Time.NS_IN_S * e.getDeltaTimeNS();
+
+        delta_x *= (destX < entity.getX()) ? -1 : 1;
+        delta_y *= (destY < entity.getY()) ? -1 : 1;
 
         int r = 0;
 
-        if(Math.abs(entity.getX() - destX) >= 0.5f && entity.moveTo(e.getMap(), entity.getX() + delta_x, entity.getY()))
+        if (Math.abs(entity.getX() - destX) >= 0.5f
+                && entity.moveTo(e.getMap(), entity.getX() + delta_x, entity.getY()))
             entity.setX(entity.getX() + delta_x);
-        else if(Math.abs(entity.getX() - destX) <= 0.5f)
+        else if (Math.abs(entity.getX() - destX) <= 0.5f)
             r++;
-        
-        if(Math.abs(entity.getY() - destY) >= 0.5f && entity.moveTo(e.getMap(), entity.getX(), entity.getY() + delta_y))
+
+        if (Math.abs(entity.getY() - destY) >= 0.5f
+                && entity.moveTo(e.getMap(), entity.getX(), entity.getY() + delta_y))
             entity.setY(entity.getY() + delta_y);
-        else if(Math.abs(entity.getY() - destY) <= 0.5f)
+        else if (Math.abs(entity.getY() - destY) <= 0.5f)
             r++;
-        
-        if(r == 2)
+
+        if (r == 2)
             this.reachedDest = true;
     }
 
-    public void reinit(){
+    public void reinit() {
 
     }
 
+    /**
+     * Returns whether the Entity has reached its destination.
+     * 
+     * @return true if the Entity has reached its destination, false otherwise
+     */
     public boolean reachedDest() {
         return this.reachedDest;
     }
-    
-    public void setDest(float destX, float destY){
+
+    /**
+     * Sets the destination for the Entity.
+     * 
+     * @param destX the x-coordinate of the destination
+     * @param destY the y-coordinate of the destination
+     */
+    public void setDest(float destX, float destY) {
         this.destX = destX;
         this.destY = destY;
     }
--- Summit/src/summit/game/ai/IceKingAI.java
+++ Summit/src/summit/game/ai/IceKingAI.java
@@ -3,19 +3,48 @@ package summit.game.ai;
 import summit.game.GameUpdateEvent;
 import summit.game.entity.Entity;
 
-public class IceKingAI extends EntityAI{
+/**
+ * 
+ * Controls the movement of the Ice King.
+ * 
+ * This class extends EntityAI and is used to control the movement of an Entity
+ * object that represents the Ice King.
+ * 
+ * @author Aadi J, Aditya B, Sanjay R, Aadithya R.
+ */
+public class IceKingAI extends EntityAI {
 
+    /**
+     * 
+     * Constructs a new IceKingAI for the specified Entity.
+     * 
+     * @param e the Entity object that this IceKingAI will control
+     */
     public IceKingAI(Entity e) {
         super(e);
     }
 
+    /**
+     * 
+     * Updates the Ice King's movement.
+     * This method should be called every game tick to update the Ice King's
+     * position.
+     * 
+     * @param e the GameUpdateEvent for the current game tick
+     * @see GameUpdateEvent
+     */
     @Override
     public void next(GameUpdateEvent e) {
-        
+
     }
 
+    /**
+     * 
+     * Re-initializes the IceKingAI.
+     * This method should be called when the Ice King is reset or re-spawned.
+     */
     @Override
     public void reinit() {
-        
+
     }
 }
